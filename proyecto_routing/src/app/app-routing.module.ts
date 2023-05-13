5fc44e29cc19b1ab0abb2cfd89637b802cf8ce70
Rafael Morales <rafael.7.morales@gmail.com>
2023-05-13 11:08:36 -0600

    capitulo 27, carga perezosa - lazy loading, carga de módulos por aparte

--- proyecto_routing/src/app/app-routing.module.ts
+++ proyecto_routing/src/app/app-routing.module.ts
@@ -4,7 +4,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  
+  {
+    path: '',
+    loadChildren: () => import('./welcome/welcome.module').then(m => m.WelcomeModule)
+  },
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+  }
 ]
 
 @NgModule({
