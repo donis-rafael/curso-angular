e7bb698fbe336c564a93c9b214d0d85ee32cdb9c
Rafael Morales <rafael.7.morales@gmail.com>
2023-05-13 18:28:06 -0600

    módulo compartido, clase 29 y final

---
+++ proyecto_routing/src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    NavigationComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    NavigationComponent,
+    FooterComponent
+  ]
+})
+export class SharedModule { }
